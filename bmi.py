90d70cb4f9c9e58f491c762616106d999334f66c
Mythreya <mythreya.23@ichat.sp.edu.sg>
2024-04-22 10:36:08 +0800

    BMI Calculator

---
+++ bmi.py
@@ -0,0 +1,16 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+
+    bmi = weight / height**2
+
+    print("Your BMI is", bmi)
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi > 25.0:
+        print("Overweight")
+    else:
+        print("Normal Weight")
+
+calculate_bmi(weight=57, height=1.73)
+
